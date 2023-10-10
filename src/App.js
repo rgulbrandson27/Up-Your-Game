c6ec8e26f21ed7715186355e8b98837c93f7df27
rgulbrandson27 <105253984+rgulbrandson27@users.noreply.github.com>
2023-10-10 10:37:31 -0500

    styling work

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import LetterBoard from './Components/LetterBoard.jsx';
 import User from './Components/User.jsx';
 import QuizBoard from './Components/QuizBoard.jsx';
+import Dashboard from './Components/Dashboard.jsx';
 import './App.css';
 import WordLists from './wordLists.js';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="status">Status: beginning Stage - in progress</h1>
-        <QuizBoard />
-        <LetterBoard />
-        <User />
+      {/* <h1 className="status">Status: beginning Stage - in progress</h1> */}
+        <Dashboard />
+        {/* <QuizBoard />
+        <LetterBoard /> */}
+        {/* <User /> */}
 
     </div>
 
--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,1 +1,23 @@
 import './Dashboard.css';
+import QuizBoard from './QuizBoard.jsx';
+import LetterBoard from './LetterBoard.jsx';
+
+const Dashboard = () => {
+
+
+    return (
+        <div className="dashboard">
+    
+       <header> </header> 
+       <body>  
+            <QuizBoard className="quiz-board-main"/>
+            <LetterBoard className="letter-board-main"/>
+
+       </body>
+       <footer>  </footer>
+       </ div>
+    )
+
+}
+
+export default Dashboard;
