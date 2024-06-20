4d2054dde8467e500db84d5872c27ba609d14037
rgulbrandson27 <105253984+rgulbrandson27@users.noreply.github.com>
2024-06-20 00:34:41 -0500

    begin hint set-up

--- src/pages/3.quizpage/Hints.jsx
+++ src/pages/3.quizpage/Hints.jsx
@@ -3,17 +3,20 @@ import React from 'react'
 import { TbHexagonNumber1, TbHexagonNumber2, TbHexagonNumber3, TbHexagonNumber4, TbHexagonNumber5 } from "react-icons/tb";
 
 
-const Hints = ({ selectedWordList }) => {
+const Hints = ({ selectedWordList, hintsRemaining }) => {
 
   return (
-    <div className='bg-green-300 h-full w-full'>
-      <h3 className='text-center my-3'>HINTS</h3>
+    <div className='bg-green-300 h-full w-full flex justify-around pt-1'>
+     
 
-      < TbHexagonNumber1 className='h-10 w-full'/>
-      < TbHexagonNumber2 className='h-10 w-full'/>  
-      < TbHexagonNumber3 className='h-10 w-full'/>  
-      < TbHexagonNumber4 className='h-10 w-full'/>
-      < TbHexagonNumber5 className='h-10 w-full'/>
+      <svg className="w-4 h-6 ml-2 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 9a3 3 0 0 1 3-3m-2 15h4m0-3c0-4.1 4-4.9 4-9A6 6 0 1 0 6 9c0 4 4 5 4 9h4Z"/>
+      </svg>
+      <h2 className="mr-2">4</h2>
+      {/* <h3>{hintsRemaining}</h3> */}
+
+
+    
     </div>
   )
 }
--- src/pages/3.quizpage/QuizPage.jsx
+++ src/pages/3.quizpage/QuizPage.jsx
@@ -10,6 +10,12 @@ import PropTypes from 'prop-types';
 const QuizPage = ({selectedWordList}) => {
 
 const [stemWordLetters, setStemWordLetters] = useState([]);
+const [hintsRemaining, setHintsRemaining] = useState([10]);
+
+
+const showHintChoices = 
+
+
 
 useEffect(() => {
   console.log("selectedWordList has passed to quizpage:", selectedWordList);
@@ -22,7 +28,7 @@ useEffect(() => {
       {/* <h2>Quiz Page</h2> */}
       {/* <p>Selected Word List: {selectedWordList ? selectedWordList.id : 'Unknown'}</p> */}
       <div className="grid grid-cols-12 grid-rows-12 w-screen h-screen flex-grow-0">
-        <h1 className="col-span-12 row-span-1 text-center text-2xl lg:text-3xl pt-2 lg:pt-3">`{selectedWordList.listName} ({selectedWordList.id})</h1>
+        <h1 className="col-span-12 row-span-1 text-center text-2xl lg:text-3xl pt-2 lg:pt-3">{selectedWordList.listName} ({selectedWordList.id})</h1>
         <div 
             className="grid row-start-2 row-span-2 col-start-3 col-span-8 
             md:row-start-4 md:row-span-2 md:col-start-2 md:col-span-6   
@@ -30,21 +36,27 @@ useEffect(() => {
             lg:col-start-3 lg:col-span-5
                 mt-4">
             <WordDisplay selectedWordList={selectedWordList} stemWordLetters={stemWordLetters}/>
-          </div>
-          <div className="correct-word-list grid col-span-6 col-start-4 row-span-8 row-start-5 mt-3 overflow-scroll
+        </div>
+
+        <div className="correct-word-list grid col-span-6 col-start-4 row-span-8 row-start-5 mt-3 overflow-scroll
           md:row-start-2 md:row-span-9 md:col-start-9 md:col-span-3
   
           lg:row-start-2 lg:rows-span-8 lg:col-start-9 lg:col-span-3 lg:mr-10">
             < CorrectWordList selectedWordList={selectedWordList} stemWordLetters={stemWordLetters}/>
-          </div>
+        </div>
 
-          
-          {/* <div className="grid row-start-2 row-span-8 col-start-11 col-span-2 mx-2">
-          < Hints /> 
-          </div> */}
+ 
+          <div className="grid row-start-4 row-span-1 col-start-6 col-span-2 border-2 border-black m-3
+          md:row-start-6 md:col-start-4 md:mx-4
+          lg:col-start-3 lg:row-start-7">
+            
+          < Hints
+              onClick={() => showHintChoices()} />
+          </div>
+          </div>
         </div>
      
-      </div>
+      
     );
   };
   
